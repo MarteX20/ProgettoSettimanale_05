a4ff60077e51c69dda5531c3f3b05c774ab072b3
MarteX20 <98113914+MarteX20@users.noreply.github.com>
2023-08-04 10:53:22 +0200

    creazione dei repository

---
+++ src/main/java/ProgettoSettimanale5/PS5Dispositivo/DispositivoRepository.java
@@ -0,0 +1,7 @@
+package ProgettoSettimanale5.PS5Dispositivo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DispositivoRepository extends JpaRepository<Dispositivo, Long> {
+
+}
